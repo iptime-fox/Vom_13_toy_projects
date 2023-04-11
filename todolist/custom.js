a71dfec978ad2e1cdce814bedb310b281f858778
iptime-fox <dudndi826@gmail.com>
2023-04-11 11:44:36 +0900

    todo alert 0411

--- todolist/custom.js
+++ todolist/custom.js
@@ -1,14 +1,32 @@
-// 1. 글 입력 후 버튼 클릭 했을 때 리스트 요소 생성
-// 2. 완료 일정 리스트 색상 표시(complete class 추가)
+// 1. 글 입력 후 버튼 클릭 했을 때 리스트 요소 생성 //
+// 2. input text null sign shows alert to text //
+// 3. 완료 일정 리스트 색상 표시(complete class 추가) //
+// 4. delete todo list //
+// 5. input null 시 엔터 한번만 진행
 
 // 요소 선택
 
 const btn = document.querySelector('.btn');
 const lists = document.querySelector('.lists');
 const inputBox = document.querySelector('.input-box');
+const alert = document.querySelector('.alert');
 
 // 초기화 변수
 let liContent = '';
+
+// alertTxt 함수 정의
+const alertTxt = () => {
+  alert.style.display = 'block';
+  const alertElement = document.createElement('p'); // p태그 생성
+  alertElement.append('Please enter your Todo List!'); // p태그에 텍스트 추가
+  alert.append(alertElement); // 텍스트가 추가된 p태그를 div 태그 내부에 추가
+
+  setTimeout(() => {
+    // alert.style.display = 'none';
+    alertElement.remove();
+    alert.style.display = 'none';
+  }, 1000); // 1초후 메시지 사라짐
+};
 
 // todo list 생성 함수 정의
 const addTodo = () => {
@@ -17,7 +35,7 @@ const addTodo = () => {
   // alert(inputValue);
   if (!inputValue) {
     // 예외처리
-    alert('Please enter your Todo List!');
+    alertTxt();
     return;
   }
 
@@ -36,8 +54,23 @@ const addTodo = () => {
 btn.addEventListener('click', addTodo);
 
 // 엔터 키보드 입력 시 버튼 클릭
-inputBox.addEventListener('keyup', function (e) {
+inputBox.addEventListener('keypress', function (e) {
   if (e.keyCode === 13) {
     addTodo();
   }
 });
+
+// define complete function
+const showComplete = (a) => {
+  // console.log(a.target.nodeName);
+  if (a.target.nodeName === 'LI') {
+    a.target.classList.toggle('complete');
+  } else if (a.target.nodeName === 'P') {
+    a.target.parentNode.classList.toggle('complete');
+  } else if (a.target.nodeName === 'I') {
+    a.target.parentNode.parentNode.remove();
+  }
+};
+
+// call complete sign function when delete button click
+lists.addEventListener('click', showComplete);
